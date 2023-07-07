dc548fb0e35812a9dd53e3c3d43f6796aad7d2ad
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-07-06 22:55:27 -0400

    change

--- string/string.js
+++ string/string.js
@@ -10,3 +10,10 @@ module.exports = {
   uppercase,
   lowercase,
 };
+
+const add = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+
+module.exports = { add, mul, sub, div };
