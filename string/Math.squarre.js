8a68bc5fcc3f0e4cd6a446df83596fbd0fb906f4
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-09-04 19:53:33 -0400

    update

--- string/Math.squarre.js
+++ string/Math.squarre.js
@@ -3,6 +3,7 @@ function adding(a, b) {
   }
   
   console.log(adding(2, 2));
+  // Expected output: 4
  
   
   function subtracting(a, b) {
@@ -10,6 +11,7 @@ function adding(a, b) {
   }
   
   console.log(subtracting(8, 2));
+  // Expected output: 6
   
   
   function multiplying(a, b) {
@@ -17,12 +19,14 @@ function adding(a, b) {
   }
   
   console.log(multiplying(2, 5));
+  // Expected output: 10
 
  function divide(a, b) {
     return Math.sqrt(a / b);
   }
   
   console.log(divide(12, 2));
+  // Expected output: 6
   
   
   
