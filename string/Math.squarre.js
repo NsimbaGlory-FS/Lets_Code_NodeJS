accecdacc8c75787ec23ddbbe3653772187e2db9
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-09-04 19:44:10 -0400

    update

---
+++ string/Math.squarre.js
@@ -0,0 +1,28 @@
+function adding(a, b) {
+    return Math.sqrt(a + b);
+  }
+  
+  console.log(adding(2, 2));
+ 
+  
+  function subtracting(a, b) {
+    return Math.sqrt(a - b);
+  }
+  
+  console.log(subtracting(8, 2));
+  
+  
+  function multiplying(a, b) {
+    return Math.sqrt(a * a);
+  }
+  
+  console.log(multiplying(2, 5));
+
+ function divide(a, b) {
+    return Math.sqrt(a / b);
+  }
+  
+  console.log(divide(12, 2));
+  
+  
+  
