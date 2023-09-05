537cf21500213026507e64f18347eb39c28e62c9
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-09-05 16:06:49 -0400

    update

--- string/Math.squarre.js
+++ string/Math.squarre.js
@@ -1,20 +1,31 @@
-function calcHypotenuse(a, b) {
-    return Math.sqrt(a * a + b * b);
-  }
-  
-  console.log(calcHypotenuse(2 , 2));
+ function adding(a, b) {
+    
+    return Math.sqrt(2, 2);
     // Expected output: 4
-   
+    let y= 4;
     
-    console.log(calcHypotenuse(8 , 2));
-    // Expected output: 6
+
+    y = Math.sqrt(y);
+
+    console.log(y);
+  } 
+
+  function adding(a, b) {
+      return Math.max(3, 4);
+      
+      let a = 3;
+      let b = 4;
+
+      let maximun;
+      
+      maximun = Math.max(a, b);
+
+      console.log(maximun);
+  }
+    
+    
   
-    
-    console.log(calcHypotenuse(2 , 5));
-    // Expected output: 10
   
-    
-    console.log(calcHypotenuse(12 , 2));
-    // Expected output: 6
-    
+  
+  
   
